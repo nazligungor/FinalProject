26fac237d1ab348d78a2aae0bf8cff793a252c29
Marley Zelinger <marley.zelinger@gmail.com>
2019-04-01 16:18:23 -0400

    moved files

--- skeleton.v
+++ skeleton.v
@@ -1,4 +1,4 @@
-module skeleton(resetn, 
+module project_skeleton(resetn, 
 	ps2_clock, ps2_data, 										// ps2 related I/O
 	debug_data_in, debug_addr, leds, 						// extra debugging ports
 	lcd_data, lcd_rw, lcd_en, lcd_rs, lcd_on, lcd_blon,// LCD info
@@ -36,10 +36,6 @@ module skeleton(resetn,
 	output 	[31:0] 	debug_data_in;
 	output   [11:0]   debug_addr;
 	
-	
-	
-	
-	
 	wire			 clock;
 	wire			 lcd_write_en;
 	wire 	[31:0] lcd_write_data;
@@ -56,7 +52,90 @@ module skeleton(resetn,
 	//assign clock = inclock;
 	
 	// your processor
-//	processor myprocessor(clock, ~resetn, /*ps2_key_pressed, ps2_out, lcd_write_en, lcd_write_data,*/ debug_data_in, debug_addr);
+	wire [11:0] address_imem;
+   wire [31:0] q_imem;
+    imem my_imem(
+        .address    (address_imem),            // address of data
+        .clock      (clock),                  // you may need to invert the clock
+        .q          (q_imem)                   // the raw instruction
+    );
+
+    /** DMEM **/
+    // Figure out how to generate a Quartus syncram component and commit the generated verilog file.
+    // Make sure you configure it correctly!
+    wire [11:0] address_dmem;
+    wire [31:0] data;
+    wire wren;
+    wire [31:0] q_dmem;
+    dmem my_dmem(
+        .address    (address_dmem),       // address of data
+        .clock      (clock),                  // may need to invert the clock
+        .data	    (data),    // data you want to write
+        .wren	    (wren),      // write enable
+        .q          (q_dmem)    // data from dmem
+    );
+
+    /** REGFILE **/
+    // Instantiate your regfile
+    wire ctrl_writeEnable;
+    wire [4:0] ctrl_writeReg;
+	 wire [4:0] ctrl_readRegA, ctrl_readRegB;
+    wire [31:0] data_writeReg;
+    wire [31:0] data_readRegA, data_readRegB;
+	 /* register information:\
+	 question --> store velocity in a register or just determine which register to read from based on 
+	 reg1: nop counter
+	 reg2: button pressed
+	 reg3: collision flag
+	 reg4: counter
+	 reg5: bird y
+	 reg6: velocity
+	 
+	 
+	 
+	 
+	 
+	 */
+	 wire [31:0] reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7, reg8, reg9, reg10, reg11, reg12, reg13, reg14, reg15, reg16, reg17, reg18, reg19, reg20, reg21, reg22, reg23, reg24, reg25, reg26, reg27, reg28, reg29, reg30, reg31;
+	 
+    regfile my_regfile(
+        clock,
+        ctrl_writeEnable,
+        ctrl_reset,
+        ctrl_writeReg,
+        ctrl_readRegA,
+        ctrl_readRegB,
+        data_writeReg,
+        data_readRegA,
+        data_readRegB,
+		  reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7, reg8, reg9, reg10, reg11, reg12, reg13, reg14, reg15, reg16, reg17, reg18, reg19, reg20, reg21, reg22, reg23, reg24, reg25, reg26, reg27, reg28, reg29, reg30, reg31
+    );
+	 
+    /** PROCESSOR **/
+    processor my_processor(
+        // Control signals
+        ~clock,                          // I: The master clock
+        ~resetn,                          // I: A reset signal
+
+        // Imem
+        address_imem,                   // O: The address of the data to get from imem
+        q_imem,                         // I: The data from imem
+
+        // Dmem
+        address_dmem,                   // O: The address of the data to get or put from/to dmem
+        data,                           // O: The data to write to dmem
+        wren,                           // O: Write enable for dmem
+        q_dmem,                         // I: The data from dmem
+
+        // Regfile
+        ctrl_writeEnable,               // O: Write enable for regfile
+        ctrl_writeReg,                  // O: Register to write to in regfile
+        ctrl_readRegA,                  // O: Register to read from port A of regfile
+        ctrl_readRegB,                  // O: Register to read from port B of regfile
+        data_writeReg,                  // O: Data to write to for regfile
+        data_readRegA,                  // I: Data from port A of regfile
+        data_readRegB                   // I: Data from port B of regfile
+    );
 	
 	// keyboard controller
 	PS2_Interface myps2(clock, resetn, ps2_clock, ps2_data, ps2_key_data, ps2_key_pressed, ps2_out);
@@ -91,5 +170,22 @@ module skeleton(resetn,
 								 .g_data(VGA_G),
 								 .r_data(VGA_R), .control(control));
 	
-	
 endmodule
+
+
+
+
+
+
+
+
+/**
+ * NOTE: you should not need to change this file! This file will be swapped out for a grading
+ * "skeleton" for testing. We will also remove your imem and dmem file.
+ *
+ * NOTE: skeleton should be your top-level module!
+ *
+ * This skeleton file serves as a wrapper around the processor to provide certain control signals
+ * and interfaces to memory elements. This structure allows for easier testing, as it is easier to
+ * inspect which signals the processor tries to assert when.
+ */
