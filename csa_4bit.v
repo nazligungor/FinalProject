13783e91b1dc26aa77020d60c8337b9e6d77feb0
Marley Zelinger <marley.zelinger@gmail.com>
2019-04-01 16:07:08 -0400

    combining files

--- csa_4bit.v
+++
@@ -1,8 +0,0 @@
-module csa_4bit(a, b, cout0, cout1, sum0, sum1);
-	input [3:0] a, b;
-	output cout0, cout1;
-	output [3:0] sum0, sum1;
-	// o is 1'b0 1 is 1'b1
-	my_rca rca0(a, b, 1'b0, cout0, sum0);
-	my_rca rca1(a, b, 1'b1, cout1, sum1);
-endmodule
--- skeleton.v
+++ skeleton.v
@@ -1,22 +1,59 @@
-/**
- * NOTE: you should not need to change this file! This file will be swapped out for a grading
- * "skeleton" for testing. We will also remove your imem and dmem file.
- *
- * NOTE: skeleton should be your top-level module!
- *
- * This skeleton file serves as a wrapper around the processor to provide certain control signals
- * and interfaces to memory elements. This structure allows for easier testing, as it is easier to
- * inspect which signals the processor tries to assert when.
- */
-
-module skeleton(clock, reset);
-    input clock, reset;
-
-    /** IMEM **/
-    // Figure out how to generate a Quartus syncram component and commit the generated verilog file.
-    // Make sure you configure it correctly!
-    wire [11:0] address_imem;
-    wire [31:0] q_imem;
+module project_skeleton(resetn, 
+	ps2_clock, ps2_data, 										// ps2 related I/O
+	debug_data_in, debug_addr, leds, 						// extra debugging ports
+	lcd_data, lcd_rw, lcd_en, lcd_rs, lcd_on, lcd_blon,// LCD info
+	seg1, seg2, seg3, seg4, seg5, seg6, seg7, seg8,		// seven segements
+	VGA_CLK,   														//	VGA Clock
+	VGA_HS,															//	VGA H_SYNC
+	VGA_VS,															//	VGA V_SYNC
+	VGA_BLANK,														//	VGA BLANK
+	VGA_SYNC,														//	VGA SYNC
+	VGA_R,   														//	VGA Red[9:0]
+	VGA_G,	 														//	VGA Green[9:0]
+	VGA_B,															//	VGA Blue[9:0]
+	CLOCK_50,
+	control);  									// 50 MHz clock
+		
+	////////////////////////	VGA	////////////////////////////
+	output			VGA_CLK;   				//	VGA Clock
+	output			VGA_HS;					//	VGA H_SYNC
+	output			VGA_VS;					//	VGA V_SYNC
+	output			VGA_BLANK;				//	VGA BLANK
+	output			VGA_SYNC;				//	VGA SYNC
+	output	[7:0]	VGA_R;   				//	VGA Red[9:0]
+	output	[7:0]	VGA_G;	 				//	VGA Green[9:0]
+	output	[7:0]	VGA_B;   				//	VGA Blue[9:0]
+	input				CLOCK_50;
+	//output         isin_pipe;
+	////////////////////////	PS2	////////////////////////////
+	input 			resetn,control;
+	input 			ps2_data, ps2_clock;
+	
+	////////////////////////	LCD and Seven Segment	////////////////////////////
+	output 			   lcd_rw, lcd_en, lcd_rs, lcd_on, lcd_blon;
+	output 	[7:0] 	leds, lcd_data;
+	output 	[6:0] 	seg1, seg2, seg3, seg4, seg5, seg6, seg7, seg8;
+	output 	[31:0] 	debug_data_in;
+	output   [11:0]   debug_addr;
+	
+	wire			 clock;
+	wire			 lcd_write_en;
+	wire 	[31:0] lcd_write_data;
+	wire	[7:0]	 ps2_key_data;
+	wire			 ps2_key_pressed;
+	wire	[7:0]	 ps2_out;	
+	//reg          x,y;
+	
+	// clock divider (by 5, i.e., 10 MHz)
+	pll div(CLOCK_50,inclock);
+	assign clock = CLOCK_50;
+	
+	// UNCOMMENT FOLLOWING LINE AND COMMENT ABOVE LINE TO RUN AT 50 MHz
+	//assign clock = inclock;
+	
+	// your processor
+	wire [11:0] address_imem;
+   wire [31:0] q_imem;
     imem my_imem(
         .address    (address_imem),            // address of data
         .clock      (clock),                  // you may need to invert the clock
@@ -45,6 +82,17 @@ module skeleton(clock, reset);
 	 wire [4:0] ctrl_readRegA, ctrl_readRegB;
     wire [31:0] data_writeReg;
     wire [31:0] data_readRegA, data_readRegB;
+	 /* register information:
+	 reg1: button pressed flag
+	 reg2: collision flag
+	 reg3: birdy
+	 
+	 
+	 
+	 
+	 
+	 
+	 */
 	 wire [31:0] reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7, reg8, reg9, reg10, reg11, reg12, reg13, reg14, reg15, reg16, reg17, reg18, reg19, reg20, reg21, reg22, reg23, reg24, reg25, reg26, reg27, reg28, reg29, reg30, reg31;
 	 
     regfile my_regfile(
@@ -63,8 +111,8 @@ module skeleton(clock, reset);
     /** PROCESSOR **/
     processor my_processor(
         // Control signals
-        ~clock,                          // I: The master clock
-        reset,                          // I: A reset signal
+        ~VGA_CLK,                          // I: The master clock
+        ~resetn,                          // I: A reset signal
 
         // Imem
         address_imem,                   // O: The address of the data to get from imem
@@ -85,5 +133,56 @@ module skeleton(clock, reset);
         data_readRegA,                  // I: Data from port A of regfile
         data_readRegB                   // I: Data from port B of regfile
     );
-
+	
+	// keyboard controller
+	PS2_Interface myps2(clock, resetn, ps2_clock, ps2_data, ps2_key_data, ps2_key_pressed, ps2_out);
+	
+	// lcd controller
+	lcd mylcd(clock, ~resetn, 1'b1, ps2_out, lcd_data, lcd_rw, lcd_en, lcd_rs, lcd_on, lcd_blon);
+	
+	// example for sending ps2 data to the first two seven segment displays
+	Hexadecimal_To_Seven_Segment hex1(ps2_out[3:0], seg1);
+	Hexadecimal_To_Seven_Segment hex2(ps2_out[7:4], seg2);
+	
+	// the other seven segment displays are currently set to 0
+	Hexadecimal_To_Seven_Segment hex3(4'b0, seg3);
+	Hexadecimal_To_Seven_Segment hex4(4'b0, seg4);
+	Hexadecimal_To_Seven_Segment hex5(4'b0, seg5);
+	Hexadecimal_To_Seven_Segment hex6(4'b0, seg6);
+	Hexadecimal_To_Seven_Segment hex7(4'b0, seg7);
+	Hexadecimal_To_Seven_Segment hex8(4'b0, seg8);
+	
+	// some LEDs that you could use for debugging if you wanted
+	assign leds = 8'b00101011;
+		
+	// VGA
+	Reset_Delay			r0	(.iCLK(CLOCK_50),.oRESET(DLY_RST)	);
+	VGA_Audio_PLL 		p1	(.areset(~DLY_RST),.inclk0(CLOCK_50),.c0(VGA_CTRL_CLK),.c1(AUD_CTRL_CLK),.c2(VGA_CLK)	);
+	vga_controller vga_ins(.iRST_n(DLY_RST),
+								 .iVGA_CLK(VGA_CLK),
+								 .oBLANK_n(VGA_BLANK),
+								 .oHS(VGA_HS),
+								 .oVS(VGA_VS),
+								 .b_data(VGA_B),
+								 .g_data(VGA_G),
+								 .r_data(VGA_R), .control(control));
+	
 endmodule
+
+
+
+
+
+
+
+
+/**
+ * NOTE: you should not need to change this file! This file will be swapped out for a grading
+ * "skeleton" for testing. We will also remove your imem and dmem file.
+ *
+ * NOTE: skeleton should be your top-level module!
+ *
+ * This skeleton file serves as a wrapper around the processor to provide certain control signals
+ * and interfaces to memory elements. This structure allows for easier testing, as it is easier to
+ * inspect which signals the processor tries to assert when.
+ */
